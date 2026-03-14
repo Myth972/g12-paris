ac5663d7b6a93f4321a3999fccc94b3765e0de7d
Myth972 <myth972@gmail.com>
2026-03-14 22:39:09 +0100

    fix: change ESM import extensions to .js for Vercel compatibility

--- api/index.ts
+++ api/index.ts
@@ -3,8 +3,8 @@ import "dotenv/config";
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 
-import { appRouter } from "../server/routers.ts";
-import { createContext } from "../server/_core/context.ts";
+import { appRouter } from "../server/routers.js";
+import { createContext } from "../server/_core/context.js";
 
 const app = express();
 app.use(express.json());
